f47664b033aeb7e219f165777ce057bd38559611
komalarao <manoharvakamullu1999@gmail.com>
2022-11-22 02:30:21 -0800

    snake and laders starting position

---
+++ StartPosition.java
@@ -0,0 +1,11 @@
+package com.bridgelabz_day4_problem;
+
+public class StartPosition {
+	public static void main(String[] args) {
+	    int position = 0;
+        System.out.println("Welcome to Snake And Ladder program");
+        System.out.println("Single player at the start position "+position);
+        
+    }
+
+}
